cd5515e3efa177c56f6c386ebd192ae623dfd963
HerbertWPS <herbert.santos@wps-sa.com.br>
2026-07-17 15:22:24 -0300

    Verificando tentativas

--- adivinhacao.cpp
+++ adivinhacao.cpp
@@ -10,8 +10,37 @@ int main(){
     cout << "* BEM-VINDOS AO JOGO DE ADIVINHACAO! *" << endl;
     cout << "**************************************" << endl;
 
-    int numero_secreto = 19;
-    cout << "O numero secreto e " << numero_secreto << " nao conte a ninguem" << endl;
+    const int NUMERO_SECRETO = 19; //É uma convenção declarar constantes sempre em capslock
+
+    //cout << "O numero secreto e " << numero_secreto << " nao conte a ninguem" << endl;
     //sempre que quisermos passar uma variavel no cout quebrar o "" e então passar a variavel entre
     // << variavel << dessa forma invés de imprimir o nome da variavel será impresso o valor da mesma
+
+    bool nao_acertou = true;
+    int tentativas = 0;
+
+    while(nao_acertou){
+        tentativas++;
+        int chute;
+        cout << "Tentativa " << tentativas << endl;
+        cout << "Qual o seu chute ";
+        cin >> chute;
+        cout << "O valor do seu chute e " << chute << endl;
+
+        bool acertou = chute == NUMERO_SECRETO;    //boas praticas colocar a condicional a ser verificada em uma variavel
+        bool chutemaior = chute > NUMERO_SECRETO;  //com nome que descreve o que ele representa para utiliza-lo no respectivo
+        bool chutemenor = chute < NUMERO_SECRETO;  //if, dessa forma o codigo fica mais facil de entender
+
+        if(acertou){
+            cout << "Parabens voce acertou o numero secreto" << endl;
+            nao_acertou = false;
+        }else if(chutemaior){
+            cout << "O seu chute e maior que o numero secreto" << endl;
+        }else{
+            cout << "O seu chute e menor que o numero secreto" << endl;
+        }
+        cout << endl;
+    }
+    cout << "Fim de jogo!" << endl;
+    cout << "Voce acertou o numero secreto em " << tentativas << " tentativas" << endl;
 }
